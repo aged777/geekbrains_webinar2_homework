826021c0f316896c06e4715ea70c9dbd8b31f1ed
Aleksandr Volkov <222volkov@gmail.com>
2023-01-18 09:11:21 +0300

    task15 finished

--- task15/Program.cs
+++ task15/Program.cs
@@ -4,3 +4,24 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("***** Программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. *****");
+Console.WriteLine(" ");
+Console.WriteLine("Введите число от 1 до 7.");
+
+int num = 0;
+
+while(true) {
+    if(!int.TryParse(Console.ReadLine(), out num) || num < 1 || num > 7)
+        Console.WriteLine("Вы ввели неверное число, введите число от 1 до 7.");
+    else break;
+}
+
+if(num > 0 && num < 6) Console.WriteLine("нет");
+else Console.WriteLine("да");
+
+Console.WriteLine(" ");
+Console.WriteLine("***** Программа завершается. Спасибо за использование) *****");
+
