582e2b9b0c7062e8d1acb21c3dbaef73a258238f
Aleksandr Volkov <222volkov@gmail.com>
2023-01-18 08:56:10 +0300

    task13 finished

--- task13/Program.cs
+++ task13/Program.cs
@@ -4,3 +4,27 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+Console.WriteLine("***** Программа принимает на вход число, на выходе показывает третью цифру этого числа, или сообщает, что третьей цифры нет. *****");
+Console.WriteLine(" ");
+Console.WriteLine("Введите число: ");
+
+int num = 0;
+
+while(true) {
+    if(!int.TryParse(Console.ReadLine(), out num))
+        Console.WriteLine("Вы ввели неверное число, введите целое число.");
+    else break;
+}
+
+int absNum = Math.Abs(num); // для работы с отрицательными числами
+
+int thirdDigit = absNum / 100 % 10;
+
+if(absNum < 100) Console.WriteLine("третьей цифры нет");
+else Console.WriteLine("Третья цифра числа " + num + " : цифра " + thirdDigit);
+
+Console.WriteLine(" ");
+Console.WriteLine("***** Программа завершается. Спасибо за использование) *****");
